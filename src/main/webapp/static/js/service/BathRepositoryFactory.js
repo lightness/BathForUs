5e85094f0a58207e1594ab887c8854e32d234d72
xual <xual@ericpol.com>
2016-03-14 11:16:01 +0300

    temp - fixed

---
+++ src/main/webapp/static/js/service/BathRepositoryFactory.js
@@ -0,0 +1,18 @@
+angular.module('myApp')
+    .factory('BathRepository', ['$http', function ($http) {
+        var url = 'bathes/';
+        return {
+            getAll: function (params) {
+                return $http.get(url, { params: params });
+            },
+            get: function (id) {
+                return $http.get(url + id);
+            },
+            save: function (bath) {
+                return $http.post(url, bath);
+            },
+            remove: function (id) {
+                return $http.delete(url + id);
+            }
+        }
+    }]);
