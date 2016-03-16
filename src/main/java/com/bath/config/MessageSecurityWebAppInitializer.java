9071d5fb9d8450e650e0b368ab1ddaff519a5bef
AlexeyShi <alexey-shitikov@mail.ru>
2016-03-16 16:31:08 +0300

    Non-working stuff related with Spring Security.

---
+++ src/main/java/com/bath/config/MessageSecurityWebAppInitializer.java
@@ -0,0 +1,7 @@
+package com.bath.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class MessageSecurityWebAppInitializer extends
+        AbstractSecurityWebApplicationInitializer {
+}
