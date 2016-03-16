1474c103a1cd93de066720e0a37004667fbb2034
xual <xual@ericpol.com>
2016-03-16 15:40:24 +0300

    Svg updated

--- src/main/webapp/static/js/app.js
+++ src/main/webapp/static/js/app.js
@@ -2,16 +2,17 @@ angular.module('myApp', ['ngRoute', 'ngMaterial', 'md.data.table', 'ngMessages',
     .config(function($routeProvider, $mdIconProvider){
         'use strict';
 
-        function View(viewPath,controller) {
-            this.templateUrl = 'static/view/' + viewPath;
-            this.controller = controller;
-        }
-
         $mdIconProvider
             .icon('add', 'static/img/svg/add.svg', 24)
             .icon('close', 'static/img/svg/close.svg', 24)
             .icon('detail', 'static/img/svg/detail.svg', 24)
-            .icon('edit', 'static/img/svg/edit.svg', 24);
+            .icon('edit', 'static/img/svg/edit.svg', 24)
+            .icon('broom', 'static/img/svg/broom.svg', 48);
+
+        function View(viewPath,controller) {
+            this.templateUrl = 'static/view/' + viewPath;
+            this.controller = controller;
+        }
 
         $routeProvider
             .when('/bathes', new View('bath/list.html', 'BathListCtrl'))
