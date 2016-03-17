6114db05d1b85b7d9da85ca69dfd96e176cdcb25
AlexeyShi <alexey-shitikov@mail.ru>
2016-03-17 22:51:24 +0300

    A bit of changes in security

--- src/main/java/com/bath/config/SecurityConfig.java
+++ src/main/java/com/bath/config/SecurityConfig.java
@@ -2,7 +2,9 @@ package com.bath.config;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -12,31 +14,37 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    private AuthenticationManagerBuilder auth;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
                     .antMatchers("/static/**").permitAll()
                     .antMatchers("/").permitAll()
+                    .antMatchers("/login").permitAll()
                     .anyRequest().authenticated()
                 .and()
-                .formLogin()
-                .loginProcessingUrl("/login")
-                .usernameParameter("username")
-                .passwordParameter("password").permitAll();
-                /*.and()
-                .logout()
-                .permitAll();*/
+                    .csrf().disable();
     }
 
     @Autowired
     public void configureGlobal(
-            AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
-        authenticationManagerBuilder
+            AuthenticationManagerBuilder auth) throws Exception {
+        auth
                 .ldapAuthentication()
-                .userDnPatterns("uid={0},ou=people")
+                .userDnPatterns("uid={0},ou=mathematicians")
                 .contextSource()
-                .root("dc=ericpol,dc=int")
+                /*.root("dc=ericpol,dc=int")
                 .url("ldap://ldap-brs1.ericpol.int:389");
+                */.root("dc=example,dc=com")
+                .url("ldap://ldap.forumsys.com:389")
+                .and().passwordCompare();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager() throws Exception {
+        return auth.build();
     }
 }
