300486bab3e69e948b541bf05a5cb60627911ec3
nvys <nvys@PC-BRS1-4-162.ad.ericpol.int>
2016-03-21 13:44:33 +0300

    add text angular editor

--- src/main/webapp/static/js/controller/BathDetailDialogCtrl.js
+++ src/main/webapp/static/js/controller/BathDetailDialogCtrl.js
@@ -1,24 +1,36 @@
 angular.module('myApp')
     .controller('BathDetailDialogCtrl', function ($scope, $mdDialog, $location, BathRepository, bath) {
-        $scope.bath = bath; 
+        $scope.bath = bath;
 
-        
-        $scope.cancel = function() {
+
+        $scope.cancel = function () {
             $mdDialog.cancel();
         };
-        $scope.answer = function(answer) {
+        $scope.answer = function (answer) {
             $mdDialog.hide(answer);                                     // !!!!!!!!!
         };
 
-        $scope.comments = [{'text': 'comment33пукпмпмпмпмпмпмпмпмпмпмпмпмпмпмпмпмпмпмпмпмпмпмпмпмпмпмпмпмпмпмпмпмпмпмпмпмпмпмпмпмпмпмпмпмпмпмпмпмпмпмпмпмпмпмпмр', 'firstName': 'Vova', 'lastName':'Oleshko', 'iconPath':'static/img/123.png'},
-            {'text': 'comment123', 'firstName': 'Vova', 'lastName':'Oleshko', 'iconPath':'static/img/123.png'},
-            {'text': 'comment3', 'firstName': 'Vova', 'lastName':'Oleshko', 'iconPath':'static/img/123.png'},
-            {'text': 'comment3', 'firstName': 'Vova', 'lastName':'Oleshko', 'iconPath':'static/img/123.png'},
-            {'text': 'comment3', 'firstName': 'Vova', 'lastName':'Oleshko', 'iconPath':'static/img/123.png'}];
+        $scope.comments = [{
+            'text': 'comment33пукпмпмпмпмпмпмпмпмпмпмпмпмпмпмпмпмпмпмпмпмпмпмпмпмпмпмпмпмпмпмпмпмпмпмпмпмпмпмпмпмпмпмпмпмпмпмпмпмпмпмпмпмпмпмпмр',
+            'firstName': 'Vova',
+            'lastName': 'Oleshko',
+            'iconPath': 'static/img/123.png'
+        },
+            {'text': 'comment123', 'firstName': 'Vova', 'lastName': 'Oleshko', 'iconPath': 'static/img/123.png'},
+            {'text': 'comment3', 'firstName': 'Vova', 'lastName': 'Oleshko', 'iconPath': 'static/img/123.png'},
+            {'text': 'comment3', 'firstName': 'Vova', 'lastName': 'Oleshko', 'iconPath': 'static/img/123.png'},
+            {'text': 'comment3', 'firstName': 'Vova', 'lastName': 'Oleshko', 'iconPath': 'static/img/123.png'}];
 
-        $scope.add = function(){
-            $scope.comments.unshift({'text': $scope.newComment, 'firstName': 'Vova', 'lastName':'Oleshko', 'iconPath':''});
+        $scope.add = function () {
+            $scope.comments.unshift({
+                'text': $scope.newComment,
+                'firstName': 'Vova',
+                'lastName': 'Oleshko',
+                'iconPath': 'static/img/123.png'
+            });
             $scope.newComment = "";
+
         };
+
 
     });
