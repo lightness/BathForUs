ecb1f26109d157c67a6f42ff24eb688b95970d47
nvys <nvys@PC-BRS1-4-162.ad.ericpol.int>
2016-03-25 14:16:22 +0300

    add Comment controller

---
+++ src/main/java/com/bath/service/CommentService.java
@@ -0,0 +1,5 @@
+package com.bath.service;
+
+public interface CommentService {
+
+}
---
+++ src/main/java/com/bath/service/CommentServiceImpl.java
@@ -0,0 +1,12 @@
+package com.bath.service;
+
+import com.bath.repository.CommentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CommentServiceImpl  implements CommentService{
+}
